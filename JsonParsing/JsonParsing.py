085c76b56a29241a47d2aadaf3951b068934a02c
jorry49 <1481215919@qq.com>
2023-12-19 22:22:31 +0800

    第二次提交

--- JsonParsing/JsonParsing.py
+++ JsonParsing/JsonParsing.py
@@ -1,116 +1,74 @@
-import json
-import numpy as np  # 确保导入了 numpy
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans
+import pandas as pd
+import datetime
+from sklearn.preprocessing import LabelEncoder
 from drain3 import TemplateMiner
 from drain3.template_miner_config import TemplateMinerConfig
-from sklearn.feature_extraction.text import CountVectorizer
 
-# 直接加载 JSON 数据
-def load_json(filepath):
-    with open(filepath, 'r', encoding='utf-8') as file:
-        return json.load(file)
+# 读取CSV文件
+def load_csv(filepath):
+    return pd.read_csv(filepath)
 
+# 解析时间戳
+def parse_timestamp(timestamp):
+    return datetime.datetime.strptime(timestamp, '%b %d, %Y @ %H:%M:%S.%f')
 
-# 使用 drain3 提取模板
-def extract_templates(data):
+# 使用 drain3 提取日志模板，并预处理 message 字段
+def extract_log_templates(data, message_column):
     config = TemplateMinerConfig()
     template_miner = TemplateMiner(config=config)
     templates = []
-    for entry in data:
-        message = entry['message']
+    for message in data[message_column]:
+        # 确保 message 是字符串类型
+        if not isinstance(message, str):
+            message = str(message)
         result = template_miner.add_log_message(message)
         templates.append(result.get("template_mined"))
     return templates
 
+# 生成序列化数据集
+def generate_sequences(data, time_window_minutes, timestamp_col, event_col):
+    sequences = []
+    start_time = data[timestamp_col].min()
+    end_time = start_time + datetime.timedelta(minutes=time_window_minutes)
 
-# 加载 JSON 数据
-data = load_json('Resource/syslog.json')
-# 提取模板
-templates = extract_templates(data)
+    while start_time < data[timestamp_col].max():
+        window_data = data[(data[timestamp_col] >= start_time) &
+                           (data[timestamp_col] < end_time)]
+        if not window_data.empty:
+            sequences.append(list(window_data[event_col]))
+        start_time = end_time
+        end_time += datetime.timedelta(minutes=time_window_minutes)
 
-# 过滤掉 None 值
-filtered_templates = [template for template in templates if template]
+    return sequences
 
-# 转换为 TF-IDF 特征
-vectorizer = TfidfVectorizer(max_features=1000)
-X = vectorizer.fit_transform(filtered_templates)
+# 主程序
+def main():
+    csv_file_path = 'Resource/routineInspection.csv'
+    log_data = load_csv(csv_file_path)
 
-# 应用 K-means 聚类
-kmeans = KMeans(n_clusters=5, random_state=0)
-kmeans.fit(X)
+    # 解析时间戳
+    log_data['parsed_timestamp'] = log_data['@timestamp'].apply(parse_timestamp)
 
+    # 使用drain3提取日志模板
+    log_data['template'] = extract_log_templates(log_data, 'message')
 
+    # 对提取的模板进行编码
+    encoder = LabelEncoder()
+    log_data['event_code'] = encoder.fit_transform(log_data['template'])
 
-# 分配的聚类标签
-labels = kmeans.labels_
+    # 生成序列化数据集
+    time_window_minutes = 5  # 可以根据需要调整时间窗口大小
+    sequences = generate_sequences(log_data, time_window_minutes, 'parsed_timestamp', 'event_code')
 
-# 输出聚类结果及分析
-for i in range(kmeans.n_clusters):
-    print(f"Cluster {i}:")
-    cluster_indices = [j for j, label in enumerate(labels) if label == i]
+    # 保存序列化数据集
+    output_file = 'Resource/data/event_sequences.txt'
+    with open(output_file, 'w') as file:
+        for sequence in sequences:
+            sequence_str = ' '.join(map(str, sequence))
+            file.write(f"{sequence_str}\n")
 
-    # 聚类的日志模板示例
-    cluster_examples = [filtered_templates[index] for index in cluster_indices]
+    print(f"Total sequences created: {len(sequences)}")
+    print(f"Sequence data saved to {output_file}")
 
-    # 分析每个聚类中的常见术语
-    vectorizer = TfidfVectorizer(max_features=1000, stop_words='english')
-    X_cluster = vectorizer.fit_transform(cluster_examples)
-    X_cluster_array = np.asarray(X_cluster.sum(axis=0))
-    common_terms = vectorizer.inverse_transform(X_cluster_array)[0]
-
-    # 打印动态生成的聚类标题
-    print("这个聚类包括以下关键词: " + ", ".join(common_terms[:5]))
-
-    # 打印每个聚类的前几个模板作为示例
-    for example in cluster_examples[:5]:  # 只显示每个聚类的前5个模板
-        print(example)
-
-    print("\n")
-
-# 这里可以保存聚类结果，以便进一步分析
-# 例如，将聚类标签和模板写入文件
-cluster_results = [{'template': template, 'cluster_label': str(label)} for template, label in
-                   zip(filtered_templates, labels)]
-with open('Resource/cluster_results.json', 'w', encoding='utf-8') as f:
-    json.dump(cluster_results, f, ensure_ascii=False, indent=4)
-
-# 使用 CountVectorizer 对模板进行标记化和构建词汇表
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(filtered_templates)
-
-# 查看词汇表
-vocabulary = vectorizer.get_feature_names_out()
-print("词汇表:")
-print(vocabulary)
-
-# 查看数值编码的结果
-print("数值编码的模板:")
-print(X.toarray())
-
-# 如果需要对特定聚类进一步分析
-for cluster_num in range(kmeans.n_clusters):
-    print(f"Cluster {cluster_num}:")
-    cluster_indices = [j for j, label in enumerate(labels) if label == cluster_num]
-
-    # 提取特定聚类的模板
-    cluster_templates = [filtered_templates[index] for index in cluster_indices]
-
-    # 使用同一个向量化器来转换这些模板
-    X_cluster = vectorizer.transform(cluster_templates)
-
-    # 查看特定聚类中的模板的数值编码
-    print(f"聚类 {cluster_num} 的数值编码:")
-    print(X_cluster.toarray())
-    print("\n")
-
-# 计算每个单词的平均 TF-IDF 得分（应该在循环外部）
-average_tfidf_scores = X.mean(axis=0)
-words = vectorizer.get_feature_names_out()
-word_scores = zip(words, average_tfidf_scores.tolist()[0])
-
-# 排序并打印得分最高的单词
-sorted_word_scores = sorted(word_scores, key=lambda x: x[1], reverse=True)
-print("平均 TF-IDF 得分最高的单词:")
-for word, score in sorted_word_scores[:10]:  # 打印前10个
-    print(f"{word}: {score}")
+if __name__ == "__main__":
+    main()
---
+++ JsonParsing/LogModelEvaluator.py
@@ -0,0 +1,141 @@
+import argparse
+import json
+
+import torch
+import torch.nn as nn
+
+# 设定设备配置
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def generate(name, window_size):
+    """
+    从数据文件中生成日志序列。
+
+    参数:
+        name (str): 数据文件名。
+        window_size (int): 日志窗口大小。
+
+    返回:
+        set: 包含日志序列的集合。
+    """
+    hdfs = set()
+    with open('data/' + name, 'r') as f:
+        for ln in f.readlines():
+            ln = list(map(lambda n: n - 1, map(int, ln.strip().split())))
+            ln = ln + [-1] * (window_size + 1 - len(ln))
+            hdfs.add(tuple(ln))
+    print('Number of sessions({}): {}'.format(name, len(hdfs)))
+    return hdfs
+
+
+class Model(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_keys):
+        super(Model, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_keys)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+
+def main():
+    # Hyperparameters
+    num_classes = 289  # 更新类别数量
+    input_size = 1
+    model_path = 'model/Adam_batch_size=2048_epoch=300.pt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-num_layers', default=2, type=int)
+    parser.add_argument('-hidden_size', default=64, type=int)
+    parser.add_argument('-window_size', default=10, type=int)
+    parser.add_argument('-num_candidates', default=9, type=int)
+    args = parser.parse_args()
+    num_layers = args.num_layers
+    hidden_size = args.hidden_size
+    window_size = args.window_size
+    num_candidates = args.num_candidates
+
+    # 模型加载
+    model = Model(input_size, hidden_size, num_layers, num_classes).to(device)
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+
+    # 数据加载，确保窗口大小一致
+    test_normal_loader = generate('hdfs_test_normal', window_size)
+    test_abnormal_loader = generate('hdfs_test_abnormal', window_size)
+
+    # 初始化统计变量
+    TP = 0  # 真正例
+    FP = 0  # 假正例
+    FN = 0  # 假反例
+    TN = 0  # 真反例
+
+    # 对正常数据进行预测
+    with torch.no_grad():
+        for line in test_normal_loader:
+            for i in range(len(line) - window_size):
+                seq = line[i:i + window_size]
+                label = line[i + window_size]
+                seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
+                label = torch.tensor(label).view(-1).to(device)
+                output = model(seq)
+                predicted = torch.argsort(output, 1)[0][-num_candidates:]
+                if label in predicted:
+                    TN += 1
+                else:
+                    FP += 1
+
+    # 对异常数据进行预测
+    with torch.no_grad():
+        for line in test_abnormal_loader:
+            for i in range(len(line) - window_size):
+                seq = line[i:i + window_size]
+                label = line[i + window_size]
+                seq = torch.tensor(seq, dtype=torch.float).view(-1, window_size, input_size).to(device)
+                label = torch.tensor(label).view(-1).to(device)
+                output = model(seq)
+                predicted = torch.argsort(output, 1)[0][-num_candidates:]
+                if label in predicted:
+                    FN += 1
+                else:
+                    TP += 1
+
+    # 计算评价指标
+    precision = TP / (TP + FP) if (TP + FP) > 0 else 0
+    recall = TP / (TP + FN) if (TP + FN) > 0 else 0  # recall也被称为真阳性率（TPR）
+    F1 = 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
+
+    # 计算真阳性率（TPR）和假阳性率（FPR）
+    TPR = TP / (TP + FN) if (TP + FN) > 0 else 0  # 真阳性率也就是召回率
+    FPR = FP / (FP + TN) if (FP + TN) > 0 else 0  # 假阳性率
+
+    metrics = {
+        'Confusion Matrix': {
+            'TP': TP,
+            'FP': FP,
+            'TN': TN,
+            'FN': FN
+        },
+        'Precision': precision,
+        'Recall': recall,  # recall is TPR
+        'F1-score': F1,
+        'True Positive Rate': TPR,  # 添加真阳性率
+        'False Positive Rate': FPR  # 添加假阳性率
+    }
+
+    # 写入到JSON文件中
+    with open('evaluation_metrics.json', 'w') as json_file:
+        json.dump(metrics, json_file)
+
+    print('Evaluation completed. Metrics:', metrics)
+
+
+# 当脚本直接运行时，调用main函数
+if __name__ == '__main__':
+    main()
